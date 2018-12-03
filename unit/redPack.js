a400b1a6fcc9cadaf7e1edb80a17117d0670344b
sky <597889510@qq.com>
2018-12-03 10:27:56 +0800

    增加天天领红包

--- unit/redPack.js
+++ unit/redPack.js
@@ -1,3 +1,29 @@
 $(function () {
-
-}
+    //生成从minNum到maxNum的随机数
+    function randomNum(minNum,maxNum){
+        switch(arguments.length){
+            case 1:
+                return parseInt(Math.random()*minNum+1,10);
+                break;
+            case 2:
+                return parseInt(Math.random()*(maxNum-minNum+1)+minNum,10);
+                break;
+            default:
+                return 0;
+                break;
+        }
+    }
+    var top=0,left=0,width=0,len=0,html="";
+    //top[0,80],left[0,90];
+   setInterval(function () {
+        left=randomNum(5,80);
+        top=randomNum(30,85);
+        width=randomNum(10,20)
+        len=$(".box img").length;
+        if(len>=6){
+            $(".box img:first-child").remove();
+        }
+        html=`<img src="img/redPack.gif" style="width:${width}%;top:${top}%;left: ${left}%"/>`;
+        $(".box").append(html);
+    },5000)
+})
